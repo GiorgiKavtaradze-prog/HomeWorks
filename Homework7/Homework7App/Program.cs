d41c911a36d7111b0777039a9c3a64e30a059e51
GiorgiKavtaradze-prog <giorgi.kavtaradze2000@mail.ru>
2026-06-08 23:14:54 +0400

    Add Homework7 project implementation

---
+++ Homework7/Homework7App/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Homework7App;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // --- Task 1
+        Console.WriteLine("Hello World!");
+    }
+}
